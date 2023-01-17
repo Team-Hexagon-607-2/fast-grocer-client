0013568a171f19cb34eeef73a04db9fecab6bdb0
Meraj <merajulhasan150@gmail.com>
2023-01-17 20:32:48 +0600

    Banner Uploaded

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { Navbar } from "./components/Navbar";
 import HomePageProducts from './components/HomePageProducts/HomePageProducts';
 import Footer from './components/Footer/Footer';
+import HomeBanner from './components/Banner/HomeBanner';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <HomeBanner></HomeBanner>
       <HomePageProducts></HomePageProducts>
       <Footer></Footer>
     </div>
