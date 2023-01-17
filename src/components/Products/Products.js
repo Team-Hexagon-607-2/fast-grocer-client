9ec6f72f25719a1749f2509f608ea206bf6d21f8
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-01-17 16:58:45 +0600

    loaded products data with loader and showing data in the ui

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
+import SingleProduct from '../Home/HomePageProducts/SingleProduct/SingleProduct';
 
 const Products = () => {
+  const products = useLoaderData();
+  console.log(products);
   return (
-    <div>
-      
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-11/12 mx-auto py-10'>
+      {
+        products?.map(product => <SingleProduct
+        key={product._id}
+        products={product}
+        ></SingleProduct>)
+      }
     </div>
   );
 };
