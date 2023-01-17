62a306e1efacf3db84d2c22ddfa575aa0f738429
saruaralam-swd <saruaralam2001@gmail.com>
2023-01-17 11:12:54 +0600

    newsletter section done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Navbar } from "./components/Navbar";
 import HomePageProducts from './components/HomePageProducts/HomePageProducts';
 import Footer from './components/Footer/Footer';
+import NewsLetter from './components/NewsLetter/NewsLetter';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <HomePageProducts></HomePageProducts>
+      <NewsLetter></NewsLetter>
       <Footer></Footer>
     </div>
   );
