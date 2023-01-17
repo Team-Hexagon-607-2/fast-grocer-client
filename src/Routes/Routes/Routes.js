2421cc0c846baf4e41a5c74b5d660c2333bc7d61
saruaralam-swd <saruaralam2001@gmail.com>
2023-01-17 20:44:47 +0600

    nothing change

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -4,16 +4,16 @@ const { default: Home } = require("../../components/Home/Home/Home");
 const { default: Main } = require("../../Layout/Main");
 
 const router = createBrowserRouter([
-    { 
+    {
         path: "/",
         element: <Main></Main>,
         errorElement: <ErrorPage></ErrorPage>,
         children: [
-            { 
+            {
                 path: "/",
                 element: <Home></Home>,
             }
-        ]        
+        ]
     }
 ]);
 
