55482aa30b3de3e7754ad70550617c2453c80989
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-01-17 17:43:48 +0600

    implemented discounted and saving amount

--- src/components/Home/HomePageProducts/SingleProduct/SingleProduct.js
+++ src/components/Home/HomePageProducts/SingleProduct/SingleProduct.js
@@ -6,7 +6,7 @@ const SingleProduct = ({ products }) => {
   // commented out console.log -by Taqi //
   // console.log(products);
   // const { product_name, product_photo, product_mesurement, product_price } = products;
-  const { name, imageUrl, price, bundle } = products;
+  const { name, imageUrl, price, bundle, original_price, save } = products;
   return (
     <div className='bg-white shadow-lg hover:shadow-2xl rounded-md border border-slate-200/60 duration-300'>
       <div className='h-225px'>
@@ -23,8 +23,12 @@ const SingleProduct = ({ products }) => {
         <div className='md:h-[60px]'>
           <p className={`text-[17px] font-semibold text-zinc-700 hover:underline cursor-pointer`}>{name.length > 50 ? name.slice(0, 50) + '...' : name}</p>
         </div>
-        <p className='text-zinc-700 font-semibold'><small>±{bundle}</small></p>
-        <p className='text-[#8ba73b] text-2xl font-bold'>৳{price}</p>
+        <p className='text-zinc-700 font-semibold'><small>{bundle ? bundle : 'As Product'}</small></p>
+        <div className='flex items-end'>
+          <p className='text-[#8ba73b] text-2xl font-bold'>৳{price}</p>
+          <p className='text-red-400 font-bold mx-1'><del>{original_price}</del></p>
+          {save.length > 0 && <p><small>(Save: {save})</small></p>}
+        </div>
         <div className='flex mt-5'>
           <button className='w-full mr-2 bg-[#9acd5e] hover:bg-[#80b248] duration-300 py-1 rounded-md flex items-center justify-center'><TbShoppingCartPlus className=' mr-2' />Add to Cart</button>
           <button className='bg-#8ba73b px-3 rounded-md bg-[#ff00001c] hover:bg-[#ff00002e] duration-300 flex items-center justify-center'><AiFillHeart className='text-red-600 mr-2' />Wishlist</button>
