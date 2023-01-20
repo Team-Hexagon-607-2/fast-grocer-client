9dbf52d0f490fc27d96de8403fe5cfa67f1d02d7
Taqinur <taqi1662@gmail.com>
2023-01-20 16:58:39 +0600

    added separate dashboard for admin and buyers. admin can delete others

--- src/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,6 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { toast } from 'react-hot-toast';
 
 const AllBuyers = () => {
+
+    const { data: users, isLoading, refetch } = useQuery({
+        queryKey: ['name'],
+        queryFn: async () => {
+            const res = await fetch('https://fg-server.vercel.app/buyers');
+            const data = await res.json();
+            return data;
+        }
+    })
+
+    
+    const handleDelete = user => {
+        console.log(user._id);
+        fetch(`https://fg-server.vercel.app/users/${user._id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount > 0){
+                refetch();
+                toast.success(`${user.name} deleted successfully`)
+            }
+        })
+    }
+
+
     return (
         <div className='my-10'>
             <h2 className="text-3xl text-yellow-700 text-center mb-4">All Buyers</h2>
@@ -17,23 +45,23 @@ const AllBuyers = () => {
                     </thead>
 
                     <tbody>
-                        
-                            {/* users?.map((user, i) => */}
+                        {
+                            users?.map((user, i) =>
                                 <tr>
                                     <td>
-                                        <div className="font-bold">i + 1</div>
+                                        <div className="font-bold">{i + 1}</div>
                                     </td>
                                     <td>
-                                        <div className="font-bold">user.name</div>
+                                        <div className="font-bold">{user.name}</div>
                                     </td>
                                     <td>
-                                        user.email
+                                        {user.email}
                                     </td>
                                     <th>
-                                        <button className="btn bg-red-600 btn-xs">Delete</button>
+                                        <button onClick={()=> handleDelete(user)}  className="btn bg-red-600 btn-xs">Delete</button>
                                     </th>
-                                </tr>
-                        
+                                </tr>)
+                        }
                     </tbody>
 
 
