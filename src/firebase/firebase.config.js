97b1d7ba1500e300d56554c33ffbd3dfa7eb63da
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-01-23 21:07:14 +0600

    change firebase auth info

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,14 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyCk6LAmJ6b3PFURJDwskLCxMMHjxTV8Ioo",
+  authDomain: "fast-grocer-01.firebaseapp.com",
+  projectId: "fast-grocer-01",
+  storageBucket: "fast-grocer-01.appspot.com",
+  messagingSenderId: "491886969293",
+  appId: "1:491886969293:web:51676c23de0da2a5aec484"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
