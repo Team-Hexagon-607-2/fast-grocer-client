1d6430e03c6a17736ac96d2525d4fad1cf1c4843
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-01-23 22:33:06 +0600

    changed dashboard layout and profile section

--- src/Layout/DashboardLayout/DashboardLayout.js
+++ src/Layout/DashboardLayout/DashboardLayout.js
@@ -22,12 +22,13 @@ const DashboardLayout = () => {
           type="checkbox"
           className="drawer-toggle"
         />
-        <div className="drawer-content">
+        <div className="drawer-content p-10">
           <Outlet></Outlet>
         </div>
         <div className="drawer-side">
           <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-          <ul className="menu mt-[77px] p-4 w-56 bg-white text-base-100">
+          <ul className="menu p-4 w-56 bg-gray-200 text-base-100">
+            <img className="w-16 h-16 mx-auto my-10 rounded-full" src={user?.photoURL || 'https://picsum.photos/200/300'} alt="" />
             <li className="rounded-none">
               <Link
                 className="border-y text-slate-700 h-[30px] py-5 !rounded-none"
--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -4,16 +4,28 @@ import { StateContext } from '../../../contexts/AuthProvider';
 const Dashboard = () => {
     const { user } = useContext(StateContext);
     return (
-        <div className='mt-10'>
-            <h2 className="text-4xl text-yellow-700 text-center mb-4">My Profile</h2>
-                <img
-                    className="mb-3 h-24 w-24 mx-auto rounded-full shadow-lg"  src={user?.photoURL || 'https://picsum.photos/200/300' }
-                     alt='' />
-                <h4 className='text-2xl text-center'>
-                    Name: {user?.displayName} <br />
-                    Email: {user?.email}
-                </h4>
-                
+        <div className=''>
+            <h2 className="text-2xl font-bold mb-4">My Profile</h2>
+            <div className='flex justify-start'>
+                <div className='mr-10 flex flex-col justify-center mx-20'>
+                    <img
+                        className="mb-1 h-32 w-32 mx-auto rounded-full shadow-lg" src={user?.photoURL || 'https://picsum.photos/200/300'}
+                        alt='' />
+                    <br />
+                    <button className='bg-[#9acd5e] hover:bg-[#80b248]'>Edit Profile</button>
+                </div>
+                <div>
+                    <div className='mb-5'>
+                        <p className='font-semibold'><small>Name</small></p>
+                        <p>{user?.displayName}</p>
+                    </div>
+                    <div className='mb-5'>
+                        <p className='font-semibold'><small>Email</small></p>
+                        <p>{user?.email}</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
