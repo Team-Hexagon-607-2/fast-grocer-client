bd6d197d01098d515540753fd6501b132e54e530
saruaralam-swd <saruaralam2001@gmail.com>
2023-01-27 19:50:27 +0600

    price filter add all product section

--- src/components/AllProducts/AllProducts.jsx
+++ src/components/AllProducts/AllProducts.jsx
@@ -1,14 +1,15 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import SingleProduct from "../Home/HomePageProducts/SingleProduct/SingleProduct";
+import sortImg from '../../assets/images/categoryModalIcon/sort.png'
 
 const AllProducts = () => {
   const [AllProducts, setAllProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
   const [totalProducts, setTotalProducts] = useState(null);
   const [size, setSize] = useState(30);
   const [page, setPage] = useState(0);
+  const [isAsc, setIsAsc] = useState('');
 
   const pages = Math.ceil(totalProducts / parseInt(size));
   const array = [...Array(pages).keys()];
@@ -30,6 +31,13 @@ const AllProducts = () => {
     );
   };
 
+  if (isAsc === 'Low Price') {
+    AllProducts.sort(function (a, b) { return a.price - b.price });
+  }
+  if (isAsc === 'High Price') {
+    AllProducts.sort(function (a, b) { return b.price - a.price });
+  }
+
   function pageIncrease() {
     if (page <= 6) {
       setPage(page + 1)
@@ -44,29 +52,38 @@ const AllProducts = () => {
 
   return (
     <>
-      <div className="flex items-center justify-center  sm:mt-[50px]">
+      <h2 className="text-center font-semibold text-2xl my-5">All Products</h2>
+
+      <div className='flex justify-end items-center mr-8'>
+        <p className='text-sm mr-2'>SORT BY</p>
+        <select onChange={(e) => setIsAsc(e.target.value)} className="select select-bordered select-sm w-56 focus:outline-none">
+          <option disabled selected>-- Price --</option>
+          <option>Low Price</option>
+          <option>High Price</option>
+        </select>
+        <img src={sortImg} alt="" />
+      </div>
+
+      <div className="flex items-center justify-center">
         {isLoading && <Loader />}
       </div>
-      <h2 className="text-center font-semibold text-2xl">All Products</h2>
-      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-11/12 mx-auto py-10">
-        {AllProducts?.map((product) => ( <SingleProduct key={product?.id} products={product} /> ))}
+
+      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-[95%] mx-auto py-5">
+        {AllProducts?.map((product) => (<SingleProduct key={product?.id} products={product} />))}
       </div>
 
-      <div className="flex justify-end mr-10 mb-10">
-        <button className={page === 7 ? 'bg-slate-300  hover:cursor-not-allowed duration-300 px-2 py-1 rounded-md' : 'bg-[#ddecb0] hover:bg-[#b9cc81] duration-300 px-2 py-1 rounded-md'} onClick={pageIncrease}>Next</button>
-        <div className="mx-2">
-          {
-            array.map(number => <button key={number.index} onClick={() => setPage(number)} className={(page === number) ? ' mx-1 w-8 h-8 rounded-full bg-[#ddecb0]' : 'bg-slate-200 mx-1 w-8 h-8 rounded-full'}>{number + 1}</button>)
-          }
-        </div>
-        <button className={page === 0 ? 'bg-slate-300  hover:cursor-not-allowed duration-300 px-2 py-1 rounded-md' : 'bg-[#ddecb0] hover:bg-[#b9cc81] duration-300 px-2 py-1 rounded-md'} onClick={pageDecrease}>Previews</button>
+      <div className="md:flex justify-between mx-8 mb-10">
+        <p>page {page + 1} of 8</p>
 
-        {/* <select onChange={(e) => setSize(e.target.value)} className='border px-2 py-1 ml-2 rounded-md'>
-          <option value="30">30</option>
-          <option value="40">40</option>
-          <option value="50">50</option>
-          <option value="60">60</option>
-        </select> */}
+        <div className="flex items-center">
+          <button className={page === 7 ? 'bg-slate-300  hover:cursor-not-allowed duration-300 px-2 py-1 text-sm rounded-md' : 'bg-[#ddecb0] hover:bg-[#b9cc81] duration-300 px-2 py-1 text-sm rounded-md'} onClick={pageIncrease}>Next</button>
+          <div className="mx-2">
+            {
+              array.map(number => <button key={number.index} onClick={() => setPage(number)} className={(page === number) ? ' mx-1 w-8 h-8 rounded-full bg-[#ddecb0]' : 'bg-slate-200 mx-1 w-8 h-8 rounded-full'}>{number + 1}</button>)
+            }
+          </div>
+          <button className={page === 0 ? 'bg-slate-300  hover:cursor-not-allowed duration-300 px-2 py-1 text-sm rounded-md' : 'bg-[#ddecb0] hover:bg-[#b9cc81] duration-300 px-2 py-1 text-sm rounded-md'} onClick={pageDecrease}>Previews</button>
+        </div>
       </div>
     </>
   );
