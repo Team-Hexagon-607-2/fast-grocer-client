698e1808487f54afdfd600afac0038cb1e173756
Ahmed-Moudud <suzan.4bd@gmail.com>
2023-02-04 00:02:28 +0600

    star raitng added

--- src/components/Review/AddReview.js
+++ src/components/Review/AddReview.js
@@ -2,60 +2,83 @@ import React from 'react';
 import { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { StateContext } from '../../contexts/AuthProvider';
+import StarRating from './StarRating';
+import { useState } from 'react';
 
 
-const AddReview = ({name,id}) => {
+const AddReview = ({ name, id }) => {
     const { register, handleSubmit, reset } = useForm();
     const { user } = useContext(StateContext);
-   
+    const [rating, setRating] = useState(null);
+
+    const date = toString(new Date().getDate());
+    const month = toString((new Date().getMonth()) + 1);
+    const year = toString(new Date().getFullYear());
 
     const submitReview = review => {
-       console.log(review);
-       const data= {...review,
-        customerName:user?.displayName,
-        customerEmail:user?.email,
-        productId:id,
-        porductName:name
+
+        const data = {
+            ...review,
+            customerName: user?.displayName,
+            customerEmail: user?.email,
+            productId: id,
+            porductName: name,
+            rating: rating,
+            time: new Date().getTime(),
+            date: date / month / year,
+             
+        }
+        console.log(data);
+
+
+        fetch('https://fg-server.vercel.app/reviews', {
+            // method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+
+
     }
-    //    console.log(data);
-      
-       fetch('https://fg-server.vercel.app/reviews', {
-        method: 'POST',
-        headers: {
-            'content-type' : 'application/json'
-        },
-        body: JSON.stringify(data)
-       })
-        
-    }
+    console.log(rating);
+
     return (
         <div className='h-[800px] flex justify-center items-center'>
             <div className='w-96 p-7'>
-                <h2 className='text-center text-xl font-bold'>Write A Review for <br/> <span className='text-accent-focus'>{name}</span></h2>
+                <h2 className='text-center text-xl font-bold'>Write A Review for <br /> <span className='text-accent-focus'>{name}</span></h2>
                 <form onSubmit={handleSubmit(submitReview)}>
+                    <div>
+                        <StarRating
+                            rating={rating}
+                            setRating={setRating}
+                        ></StarRating>
+                    </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text font-bold">Product Quality</span>
-                            </label>
-                            <select  className="select select-bordered w-full"
-                         {...register("rating",  { required: true })}>
+                        </label>
+                        <select className="select select-bordered w-full"
+                            {...register("quality", { required: true })}>
                             <option value="Excellent">Excellent</option>
-                            <option value="good">Good</option>                            
+                            <option value="good">Good</option>
                             <option value="poor">Poor</option>
                         </select>
-                       
 
                     </div>
+
+
                     <div className="form-control w-full max-w-xs mt-2">
                         <label className="label">
                             <span className="label-text font-bold">Share your experience</span>
-                            </label>
-                            <textarea type='text'
-                             {...register("feedback",{
+                        </label>
+                        <textarea type='text'
+                            {...register("feedback", {
                                 required: false
-                             })}
-                             className="textarea border border-slate-300" placeholder="We always evaluate your feedback"></textarea>
-                    
+                            })}
+                            className="textarea border border-slate-300" placeholder="We always evaluate your feedback"></textarea>
+
                     </div>
                     <input className='btn btn-accent w-full mt-4' value="Submit" type="submit" />
                 </form>
--- src/components/Review/SingleReview.js
+++ src/components/Review/SingleReview.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import ReactStars from 'react-star-rating-component';
 
 const SingleReview = ({review}) => {
     const {rating, feedback} = review;
     return (
         <div className='grid grid-cols-3'>
-      <div>{rating}</div>
+      <div>
+        <ReactStars
+         starCount={rating}
+         editing={false}
+         emptyStarColor={"#ffc107"}
+        ></ReactStars>
+      </div>
       <div>{feedback}</div>
       
       </div>
