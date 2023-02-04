681abdda3383c6e740211c51d604cf14eef85171
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-02-04 14:28:51 +0600

    modification of no product found text in search page

--- src/components/Shared/Navbar/NavLinks.jsx
+++ src/components/Shared/Navbar/NavLinks.jsx
@@ -10,28 +10,28 @@ import { NavLink } from "react-router-dom";
 const NavNavLinks = () => {
 
   return (
-    <div className="flex flex-row flex-wrap sm:gap-2 text-[14px] py-2 text-white">
+    <div className="flex flex-row flex-wrap text-[14px] py-2 text-white">
         <NavLink to="/" >
           <label
             tabIndex={0}
-            className="cursor-pointer mx-3"
+            className="cursor-pointer hover:bg-slate-700 py-2 px-4 duration-300"
           >
             Home
           </label>
         </NavLink>
 
         <NavLink to="/allproducts">
-          <label className="cursor-pointer mx-3">
+          <label className="cursor-pointer hover:bg-slate-700 py-2 px-4 duration-300">
             All Products
           </label>
         </NavLink>
 
       <NavLink to="/onsale">
-        <label className="cursor-pointer mx-3">Offer</label>
+        <label className="cursor-pointer hover:bg-slate-700 py-2 px-4 duration-300">Offer</label>
       </NavLink>
 
       <NavLink to="/aboutUs">
-        <label className="cursor-pointer mx-3">About Us</label>
+        <label className="cursor-pointer hover:bg-slate-700 py-2 px-4 duration-300">About Us</label>
       </NavLink>
     </div>
   );
