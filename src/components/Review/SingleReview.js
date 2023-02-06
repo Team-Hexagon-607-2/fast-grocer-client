b05cddf5318870e873d25f66bc2c629681be7664
Taqinur Tahmid <taqi1662@gmail.com>
2023-02-06 12:54:15 +0600

    Revert "Moududahmed"

--- src/components/Review/SingleReview.js
+++ src/components/Review/SingleReview.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import ReactStars from 'react-star-rating-component';
 
 const SingleReview = ({review}) => {
     const {rating, feedback} = review;
     return (
         <div className='grid grid-cols-3'>
-      <div>
-        <ReactStars
-         starCount={rating}
-         editing={false}
-         emptyStarColor={"#ffc107"}
-        ></ReactStars>
-      </div>
+      <div>{rating}</div>
       <div>{feedback}</div>
       
       </div>
