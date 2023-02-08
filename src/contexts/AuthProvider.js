a03275a8589472162b58763ff6422554185e95f2
saruaralam-swd <saruaralam2001@gmail.com>
2023-02-09 00:09:23 +0600

    user logout access token remove

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -182,6 +182,7 @@ export const ContextProvider = ({ children }) => {
   const logOut = () => {
     setLoading(true);
     toast.success("Logout Successfully");
+    localStorage.removeItem('accessToken')
     return signOut(auth);
   };
 
