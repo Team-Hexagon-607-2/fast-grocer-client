67fb13c611f724c40dd9f8d10adbf6f145d4c59a
saruaralam-swd <saruaralam2001@gmail.com>
2023-02-16 00:35:01 +0600

    only buyer and non-user can see wishlist icon in header

--- src/components/Shared/Navbar/Wishlist.jsx
+++ src/components/Shared/Navbar/Wishlist.jsx
@@ -1,11 +1,3 @@
-/**
- * author : md atiqul islam
- * this file contain desktop navbar wishlist
- * user can love product add wishlist and see wishlist
- * quantity and navigate to wishlist page
- *
- */
-
 import React from "react";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
@@ -13,21 +5,16 @@ import { StateContext } from "../../../contexts/AuthProvider";
 import { AiFillHeart } from "react-icons/ai";
 
 const Wishlist = () => {
-  const { wishListData } = useContext(StateContext);
+  const { isAdmin, isDeliveryman, wishListData } = useContext(StateContext);
+  
   return (
-    <Link to="/dashboard/my-wishlist" className="flex flex-row">
+    <Link to="/dashboard/my-wishlist" className={isAdmin || isAdmin ? "flex flex-row hidden" : "flex flex-row"}>
       <label tabIndex={0} className="btn btn-ghost btn-circle">
         <div className="indicator">
-        <AiFillHeart className="text-2xl" />
+          <AiFillHeart className="text-2xl" />
           <span className="badge badge-sm indicator-item">{wishListData?.data?.length}</span>
         </div>
       </label>
-      {/* <div className="w-[40px] h-[40px] rounded-[50%] hover:text-[#96B240]  bg-[#F2F4EC] flex items-center justify-center ">
-        <BsHeart className="text-lg hover:text-[#96B240] " />
-      </div>
-      <div className="badge -ml-[8px] bg-[#F6A64D] border-none rounded-full w-[25px] h-[25px] font-bold">
-        {wishListData?.data?.length}
-      </div> */}
     </Link>
   );
 };
