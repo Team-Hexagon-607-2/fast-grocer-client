99e265a92a787788c7a8f764e9fe27a3ddca8b5d
saruaralam-swd <saruaralam2001@gmail.com>
2023-02-16 01:58:42 +0600

    banner image change

--- src/components/Home/HomeBanner/HomeBanner.js
+++ src/components/Home/HomeBanner/HomeBanner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import img1 from '../../../assets/images/banner/1.jpg';
-import img2 from '../../../assets/images/banner/2.jpg';
+import img2 from '../../../assets/images/banner/2.webp';
 import img3 from '../../../assets/images/banner/3.jpg';
 import img4 from '../../../assets/images/banner/4.jpg';
 import HomeBannerItem from './HomeBannerItem';
